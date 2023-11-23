8cf3dd4e4116bd21611fa52dfc6d69aebc9fd762
kyrafenrir <erikamp1112@gmail.com>
2023-11-23 10:33:41 +0100

    Versión 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Proyecto DAW
+## Autor
+Erika Martínez Pérez
+## Requisitos minimos del desarrollo
+- [Apache2.4.52]
+- [MySQL8.0.35]
+- [PHP8.1]
+- [XDebug3.1.2]
+## Descripcion
+Documentacion de la asignatura desarrollo web en entorno servidor / DWES
+## Funcionalidades
+## Entorno de desarrollo
+[http://daw202.isauces.local/202DWESProyectoDWES/indexProyectoDWES.php]
+(Entorno de desarrollo de Erika)
+## Entorno de explotacion
+[http://daw202.ieslossauces.local/202DWESProyectoDWES/indexProyectoDWES.php]
+(Entorno de explotación de Erika)
---
+++ indexProyectoDWES.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="./webroot/css/style.css">
+        <title>Erika Martínez Pérez - DWES</title>
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo web en entorno servidor</h1>
+        </header>
+        <main>
+            <ol>
+                <li>
+                    <strong>TEMA 1:</strong> DESARROLLO WEB EN ENTORNO SERVIDOR
+                    <ul class="menu">
+                        <li><a href="doc/EjerciciosTema1.pdf" target="_blank">ESTUDIO TEORICO SOBRE DESARROLLO DE APLICACIONES WEB</a></li>
+                        <li><a>ESTUDIO ARQUITECTURA LARAVEL / SPRING BOOT</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <strong>TEMA 2:</strong> INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DE LOS ENTORNOS
+                    <ul class="menu">
+                        <li><a href="doc/EjerciciosTema2.pdf" target="_blank">DOCUMENTACION ENTORNO DE DESARROLLO Y EXPLOTACION EN PHP</a></li>
+                        <li><a>ESTUDIO DE CLASES Y FUNCIONES PHP</a></li>
+                    </ul>
+                </li>
+                <li><a href="/202DWESProyectoTema3/indexProyectoTema3.php"><strong>TEMA 3:</strong> CARACTERÍSTICAS DEL LENGUAJE PHP</a></li>
+                <li><a href="/202DWESProyectoTema4/indexProyectoTema4.php"><strong>TEMA 4:</strong> TÉCNICAS DE ACCESO A DATOS EN PHP</a></li>
+                <li><a href="/202DWESProyectoTema5/indexProyectoTema5.php"><strong>TEMA 5:</strong> DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</a></li>
+            </ol>
+        </main>
+        <footer>
+            <p>2023-2024 © Todos los derechos reservados. <a href="../index.html">Erika Martínez Pérez</a></p>
+        </footer>
+    </body>
+</html>
